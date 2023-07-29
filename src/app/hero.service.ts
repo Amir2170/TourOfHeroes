581ab9513f09a4591739dfcc18a9aed7742d9a7b
Amir2170 <amirrezaamp2170@gmail.com>
2023-07-29 01:54:13 -0700

    working detail component to view hero details

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,10 @@ export class HeroService {
     this.messageService.add("HeroComponent: heroes fetched");
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero | undefined>{
+    const hero = HEROES.find(hero => hero.id === id);
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
